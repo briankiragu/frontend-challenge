8c7c1fd0bbc5b6344d25ae6a820adf97a46667ec
David Macharia <meckamsee@gmail.com>
2022-08-17 14:52:09 +0300

    fix: update model syntax

--- app/models/task.js
+++ app/models/task.js
@@ -1,5 +1,5 @@
-import DS from 'ember-data';
-const { Model, attr } = DS;
+import Model from '@ember-data/model';
+import { attr } from '@ember-data/model';
 
 export default class TaskModel extends Model {
   @attr('string')
